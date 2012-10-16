18b91d6e9cd3d7ea58c003ab936a4ad567b2a66b
Fernando Correa <fernandocorrea@gmail.com>
2012-10-16 19:30:25 -0300

    Start working on chrome

--- Depender.js
+++ Depender.js
@@ -1,5 +1,7 @@
 function Depender() {
 	this.loaded_list = [];
+	this.callbacks   = [];
+	if(!this.container) this.container = document.body;
 }
 
 Depender.getInstance = function() {
@@ -12,36 +14,35 @@ Depender.prototype = {
 	waiting_downloads:	0,
 	loaded_list:		null,
 	download:		function(url, callback) {
-		var AJAX = new XMLHttpRequest();
-		if (AJAX) {
-			var _this = this;
-			AJAX.open("GET", url, true);                             
-			AJAX.onreadystatechange = function(){
-console.log("running:");
-console.log("readState: " + AJAX.readState);
-console.log("status   : " + AJAX.status);
-				if(AJAX.readyState == 4 && AJAX.status == 200)
-					callback(AJAX.responseText);
+		var xmlHttpRequest = new XMLHttpRequest();
+		if (xmlHttpRequest) {
+			xmlHttpRequest.open("GET", url, true);                             
+			xmlHttpRequest.onreadystatechange = function(){
+				if((this.readyState == 4 && this.status == 200) || !this.status)
+					callback(this.responseText);
 			};
-			AJAX.send(null);
+			xmlHttpRequest.send(null);
 		}
 	},
 	create_script_tag:	function(container) {
-		if(!container) container = document.body;
 		var script_tag = document.createElement("script");
-		container.appendChild(script_tag);
+		this.container.appendChild(script_tag);
 		return script_tag;
 	},
 	depends:		function(url, callback) {
-		var _this = this;
-		var script_tag = _this.create_script_tag();
-		_this.waiting_downloads++;
-		_this.download(url, function(response){
-			script_tag.innerHTML = response;
-			_this.loaded_list.push(url);
-			_this.waiting_downloads--;
-			if(_this.waiting_downloads == 0)
-				callback();
+		this.callbacks.push(callback);
+		var script_tag = this.create_script_tag();
+		this.waiting_downloads++;
+		this.loaded_list.push(url);
+		this.download(url, function(response){
+			script_tag.innerHTML = response + ";				\
+				Depender.getInstance().waiting_downloads--;		\
+				if(Depender.getInstance().waiting_downloads == 0) {	\
+					var cb = Depender.getInstance().callbacks.pop();\
+					if(cb.constructor == Function)			\
+						cb.call();				\
+				}							\
+			";
 		});
 	},
 	already_loaded:		function(url) {
